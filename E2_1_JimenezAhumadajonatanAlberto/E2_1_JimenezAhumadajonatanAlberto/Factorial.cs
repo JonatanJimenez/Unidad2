98d9979c7304265d2e41608407631761a967da57
JonatanJimenez <43358992+JonatanJimenez@users.noreply.github.com>
2018-09-28 01:29:28 -0700

    E2-1Factorial

---
+++ E2_1_JimenezAhumadajonatanAlberto/E2_1_JimenezAhumadajonatanAlberto/Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_1_JimenezAhumadajonatanAlberto
+{
+    class Factorial
+    {
+        public int FactorialFor()
+        {
+            int resultado = 1;
+            for (int contador = 1; contador < 7; contador++)
+            {
+                resultado = resultado * contador;
+            }
+
+            return resultado;
+        }
+
+        public int FactorialRecursividad(int numero = 6)
+        {
+            if (numero == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return numero * FactorialRecursividad(numero - 1);
+            }
+        }
+    }
+}
---
+++ E2_1_JimenezAhumadajonatanAlberto/E2_1_JimenezAhumadajonatanAlberto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_1_JimenezAhumadajonatanAlberto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial();
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
+            int resultadoFor = factorial.FactorialFor();
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine("Resultado del Factorial del numero 6 con For: {0} ", resultadoFor);
+            Console.WriteLine("Tiempo de Ejecucion: {0} Milisegundos", stop.Subtract(start).TotalMilliseconds);
+
+            TimeSpan parar;
+            TimeSpan iniciar = new TimeSpan(DateTime.Now.Ticks);
+            int resultadoRecursividad = factorial.FactorialRecursividad();
+            parar = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine("\nResultado del Factorial del numero 6 con Recursividad: {0}\n", resultadoRecursividad);
+            Console.WriteLine("Tiempo de Ejecucion: {0} Milisegundos", parar.Subtract(iniciar).TotalMilliseconds);
+        }
+    }
+}
